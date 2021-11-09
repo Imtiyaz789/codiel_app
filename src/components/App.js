59bf1c6f25e7f0a339f07f2cee400e54d080429e
Mohemmad Imtiyaz <imtiyajkhan789@gmail.com>
2021-11-09 19:33:09 +0530

    added action, reducer for post showing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import {fetchPosts} from '../actions/posts'
+class App extends React.Component {
 
-
-function App() {
-  return (
-    <div className="App">
-     <h1>Codiel App</h1>
-    </div>
-  );
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+  
+  render() {
+    // console.log("props", this.props);
+    const {posts} = this.props;
+    return (
+      <>
+        <div className="posts-list">
+          {posts.map(post => (
+            <div className="post-wrapper" key={post.id}>
+              <div className="post-header">
+                <div className="post-avatar">
+                  <img src="https://cdn-icons.flaticon.com/png/512/3177/premium/3177440.png?token=exp=1636466923~hmac=ef623baf7906428b1ce1173e2088a74e" alt="user post"/>
+                  <div>
+                    <span className="post-author">{post.user.name}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps(state){
+  return{
+    posts:state.posts,
+  }
+}
+
+export default connect(mapStateToProps) (App);
